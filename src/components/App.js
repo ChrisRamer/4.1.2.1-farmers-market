90d18175b49c9ec0533d1499c958c967e6f5c44a
Chris Ramer <chrisramer.97@gmail.com>
2021-09-28 11:45:12 -0700

    Add my location section

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import MyLocation from "./Location";
 
 function App() {
   return (
     <React.Fragment>
 		<Header />
+		<MyLocation />
 	</React.Fragment>
   );
 }
---
+++ src/components/Location.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyLocation() {
+	return (
+		<div id="location_section" class="panel">
+			<p>My location</p>
+		</div>
+	);
+}
+
+export default MyLocation;

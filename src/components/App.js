556d64f8d2a4e0e4718d32ef8ea24d7162a4724f
Chris Ramer <chrisramer.97@gmail.com>
2021-09-29 11:56:21 -0700

    Add produce section

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import MyLocation from "./Location";
+import AvailableProduce from "./Produce";
 
 function App() {
   return (
     <React.Fragment>
 		<Header />
 		<MyLocation />
+		<AvailableProduce />
 	</React.Fragment>
   );
 }
